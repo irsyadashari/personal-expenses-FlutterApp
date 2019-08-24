6654297d230017cdcb4ce41dc8ee586cea37ef01
irsyad <irsyad.ashari17@gmail.com>
2019-08-24 17:25:30 +0800

    membuat list transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/widgets/transaction_list.dart';
+import './widgets/chart.dart';
 import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 import 'models/transaction.dart';
@@ -50,25 +50,18 @@ class _MyHomePageState extends State<MyHomePage> {
     //   amount: 16.99,
     //   date: DateTime.now(),
     // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 16.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 16.99,
-    //   date: DateTime.now(),
-    // ),
-    // Transaction(
-    //   id: 't2',
-    //   title: 'Weekly Groceries',
-    //   amount: 16.99,
-    //   date: DateTime.now(),
-    // ),
   ];
+
+  List<Transaction> get _recentTransactions{
+    return _userTransactions.where( (tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+          ),
+      );
+    }
+    ).toList();
+  }
 
   void _addNewTransaction(String txtitle, double txAmount) {
     final newTx = Transaction(
@@ -117,13 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
           //mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('CHART'),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
